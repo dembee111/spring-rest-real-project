6aed0f4145b2935f7975fc307380eb0d2ea46873
Dembee <dembee@Dembees-MacBook-Pro.local>
2023-04-21 10:46:30 +0800

    ===== spring security local secure =====

--- src/main/java/com/dembee/springboot/springbootrestcrudemployee/dao/EmployeeRepository.java
+++ src/main/java/com/dembee/springboot/springbootrestcrudemployee/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.dembee.springboot.springbootrestcrudemployee.dao;
 
 import com.dembee.springboot.springbootrestcrudemployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //No need code this here
 }
